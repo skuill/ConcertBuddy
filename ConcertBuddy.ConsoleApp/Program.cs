b73453b96989de41e22df9fddf90eea715ea2a26
Виталий Квашин <sativkv@gmail.com>
2021-12-07 16:45:58 +0300

    Created simple console app

---
+++ ConcertBuddy.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SetlistFmAPI;
+using SetlistFmAPI.Models;
+
+namespace ConcertBuddy.ConsoleApp
+{
+    public class Program
+    {
+        // Spotify
+        // Client ID c75856d9e7454368aca6fe620a103d29
+
+
+        public static void Main(string[] args)
+        {
+            string bandName = "Parkway Drive";
+
+            // sativkv@gmail.com API
+            string setlistApiKey = "YswjYvgRtNyqt6zHfVQavkh8uyf_iP6ZWQjv";
+
+            ISetlistFmClient setlistFmClient = new SetlistFmClient(setlistApiKey);
+            
+            var artists = setlistFmClient.SearchArtists(bandName).GetAwaiter().GetResult();
+            var artist = artists.Items.FirstOrDefault();
+
+            var setlists = setlistFmClient.SearchArtistSetlists(artist.MBID).GetAwaiter().GetResult();
+            Console.WriteLine($"setlists: {setlists.Items.Count}");
+
+        }
+    }
+}
